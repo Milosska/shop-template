8a755e2cae29547a27b7625ddbe1b52ba4f90bac
Milosska <110258611+Milosska@users.noreply.github.com>
2023-09-27 19:59:47 +0300

    add base vite bundle and basic styles
    Creates app with a help of Vite bundler. Adds modern-normalize to
    normalize styles and styled-components to create global basic style
    normalization.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "modern-normalize";
+import { GlobalStyles } from "./GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Shop Template</h1>
+    </>
+  );
+}
+
+export default App;
